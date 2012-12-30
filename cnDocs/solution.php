b4357fbba1511e28fb01e25d36ba234ed8630b7a
cloudAi <cloudAi@qq.com>
2012-12-31 00:45:29 +0800

    update solutions center-middle

--- cnDocs/solution.php
+++ cnDocs/solution.php
@@ -16,78 +16,78 @@
   <div class="container">
      <!-- <div class="span12">-->
         
-        <!-- Overview
-        ================================================== -->
-        <!--
-        <section id="overview">
-            <div class="page-header">
-                <h1>概述</h1>
-            </div>
-            <p>有时候一个小小的问题，可能需要调试很久或者是每个人遇到都要去调整，那么这都是些什么问题，下面列出了最常用的解决方案，目前仍在规划制作中...</p>
-        </section>
-        <div class="page-header">
-            <h1></h1>
-        </div>
-        -->
-        
-        <!-- Solutions
-        ================================================== -->
-        <section id="solutions">
-        	
-            <h3>浏览器兼容解决方案 <small>头痛的问题，我们提前搞定</small></h3>
-            <p><a href="solutions.html">solutions</a></p>
-            <!-- 预添加简洁分类筛选 Todo -->
+	<!-- Overview
+	================================================== -->
+	<!--
+	<section id="overview">
+		<div class="page-header">
+			<h1>概述</h1>
+		</div>
+		<p>有时候一个小小的问题，可能需要调试很久或者是每个人遇到都要去调整，那么这都是些什么问题，下面列出了最常用的解决方案，目前仍在规划制作中...</p>
+	</section>
+	<div class="page-header">
+		<h1></h1>
+	</div>
+	-->
+	
+	<!-- Solutions
+	================================================== -->
+	<section id="solutions">
+		
+		<h3>浏览器兼容解决方案 <small>头痛的问题，我们提前搞定</small></h3>
+		<p><a href="solutions.html">solutions</a></p>
+		<!-- 预添加简洁分类筛选 Todo -->
+		
+		<ul class="thumbnails case">
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/center-middle.html"><img src="../solutions/img/center-middle.png" alt="垂直/水平居中" ><h4>垂直/水平居中</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/select-mask.html"><img src="../solutions/img/select-mask.png" alt="IE6 浮层遮盖" ><h4>IE6 浮层遮盖</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/rotate.html"><img src="../solutions/img/rotate.png" alt="跨浏览器翻转" ><h4>跨浏览器翻转</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/remove-outline.html"><img src="../solutions/img/outline.png" alt="" ><h4>去除虚线框</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/position-fixed.html"><img src="../solutions/img/position-fixed.png" alt="IE6 position:fixed" ><h4>IE6 position:fixed</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/pngfix.html"><img src="../solutions/img/png24.png" alt="IE6 PNG 半透明" ><h4>IE6 PNG 半透明</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/placeholder.html"><img src="../solutions/img/placeholder.png" alt="跨浏览器 Placeholder" ><h4>跨浏览器 Placeholder</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/min-max.html"><img src="../solutions/img/min-max.png" alt="IE6 最大/小宽度" ><h4>IE6 最大/小宽度</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/localStorage.html"><img src="../solutions/img/solution-default.png" alt="跨浏览器文本存储" ><h4>跨浏览器文本存储</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/linear-gradient.html"><img src="../solutions/img/linear-gradient.png" alt="跨浏览器线性渐变" ><h4>跨浏览器线性渐变</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/ime-disabled.html"><img src="../solutions/img/ime.png" alt="强制非中文输入法" ><h4>强制非中文输入法</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/ie-z-index-bug.html"><img src="../solutions/img/z-index.png" alt="IE z-index bug" ><h4>IE z-index bug</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/force-wrap.html"><img src="../solutions/img/solution-default.png" alt="文本强制换行" ><h4>文本强制换行</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/font-face.html"><img src="../solutions/img/font-face.png" alt="@font-face" ><h4>@font-face</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/float-center.html"><img src="../solutions/img/float-center.png" alt="float:center" ><h4>float:center</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/equal-height-layout.html"><img src="../solutions/img/layout.png" alt="布局等高栏" ><h4>布局等高栏</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/drop-shadow.html"><img src="../solutions/img/box-shadow.png" alt="跨浏览器投影" ><h4>跨浏览器投影</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/custom-file.html"><img src="../solutions/img/upload.png" alt="自定义上传文件样式" ><h4>自定义上传文件样式</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/cross-domain-iframe.html"><img src="../solutions/img/solution-default.png" alt="iframe 跨域自适应" ><h4>iframe 跨域自适应</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/cross-browers-copy.html"><img src="../solutions/img/solution-default.png" alt="跨浏览器剪贴功能" ><h4>跨浏览器剪贴功能</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/1px-round-corner.html"><img src="../solutions/img/radius-1px-box.png" alt="一像素圆角" ><h4>一像素圆角</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/layout"><img data-src="holder.js/180x120" alt="" ><h4>间隔</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/layout"><img data-src="holder.js/180x120" alt="" ><h4>布局</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/border"><img data-src="holder.js/180x120" alt="" ><h4>border</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/clear-float"><img data-src="holder.js/180x120" alt="" ><h4>清除浮动</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/ellipsis"><img data-src="holder.js/180x120" alt="" ><h4>省略号</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/imgtext"><img data-src="holder.js/180x120" alt="" ><h4>图文混排</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/itemlist"><img data-src="holder.js/180x120" alt="" ><h4>项目列表</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/newslist"><img data-src="holder.js/180x120" alt="" ><h4>新闻列表</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/tabs"><img data-src="holder.js/180x120" alt="" ><h4>tab标题</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/radio-checkbox"><img data-src="holder.js/180x120" alt="" ><h4>radio/checkbox</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/button-submit"><img data-src="holder.js/180x120" alt="" ><h4>button/submit</h4></a></li>
 			
-            <ul class="thumbnails case">
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/center-middle"><img src="../solutions/img/vertical-middle.png" alt="垂直/水平居中" ><h4>垂直/水平居中</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/select-mask.html"><img src="../solutions/img/select-mask.png" alt="IE6 浮层遮盖" ><h4>IE6 浮层遮盖</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/rotate.html"><img src="../solutions/img/rotate.png" alt="跨浏览器翻转" ><h4>跨浏览器翻转</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/remove-outline.html"><img src="../solutions/img/outline.png" alt="" ><h4>去除虚线框</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/position-fixed.html"><img src="../solutions/img/position-fixed.png" alt="IE6 position:fixed" ><h4>IE6 position:fixed</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/pngfix.html"><img src="../solutions/img/png24.png" alt="IE6 PNG 半透明" ><h4>IE6 PNG 半透明</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/placeholder.html"><img src="../solutions/img/placeholder.png" alt="跨浏览器 Placeholder" ><h4>跨浏览器 Placeholder</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/min-max.html"><img src="../solutions/img/min-max.png" alt="IE6 最大/小宽度" ><h4>IE6 最大/小宽度</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/localStorage.html"><img src="../solutions/img/solution-default.png" alt="跨浏览器文本存储" ><h4>跨浏览器文本存储</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/linear-gradient"><img src="../solutions/img/linear-gradient.png" alt="跨浏览器线性渐变" ><h4>跨浏览器线性渐变</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/ime-disabled"><img src="../solutions/img/ime.png" alt="强制非中文输入法" ><h4>强制非中文输入法</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/ie-z-index-bug"><img src="../solutions/img/z-index.png" alt="IE z-index bug" ><h4>IE z-index bug</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/force-wrap"><img src="../solutions/img/solution-default.png" alt="文本强制换行" ><h4>文本强制换行</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/font-face"><img src="../solutions/img/font-face.png" alt="@font-face" ><h4>@font-face</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/float-center"><img src="../solutions/img/float-center.png" alt="float:center" ><h4>float:center</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/equal-height-layout"><img src="../solutions/img/layout.png" alt="布局等高栏" ><h4>布局等高栏</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/drop-shadow"><img src="../solutions/img/box-shadow.png" alt="跨浏览器投影" ><h4>跨浏览器投影</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/custom-file"><img src="../solutions/img/upload.png" alt="自定义上传文件样式" ><h4>自定义上传文件样式</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/cross-domain-iframe"><img src="../solutions/img/solution-default.png" alt="iframe 跨域自适应" ><h4>iframe 跨域自适应</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/cross-browers-copy"><img src="../solutions/img/solution-default.png" alt="跨浏览器剪贴功能" ><h4>跨浏览器剪贴功能</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/1px-round-corner"><img src="../solutions/img/radius-1px-box.png" alt="一像素圆角" ><h4>一像素圆角</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/layout"><img data-src="holder.js/180x120" alt="" ><h4>间隔</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/layout"><img data-src="holder.js/180x120" alt="" ><h4>布局</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/border"><img data-src="holder.js/180x120" alt="" ><h4>border</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/clear-float"><img data-src="holder.js/180x120" alt="" ><h4>清除浮动</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/ellipsis"><img data-src="holder.js/180x120" alt="" ><h4>省略号</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/imgtext"><img data-src="holder.js/180x120" alt="" ><h4>图文混排</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/itemlist"><img data-src="holder.js/180x120" alt="" ><h4>项目列表</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/newslist"><img data-src="holder.js/180x120" alt="" ><h4>新闻列表</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/tabs"><img data-src="holder.js/180x120" alt="" ><h4>tab标题</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/radio-checkbox"><img data-src="holder.js/180x120" alt="" ><h4>radio/checkbox</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/button-submit"><img data-src="holder.js/180x120" alt="" ><h4>button/submit</h4></a></li>
-                
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/form"><img data-src="holder.js/180x120" alt="" ><h4>form表单</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/copyhtml"><img data-src="holder.js/180x120" alt="" ><h4>html固定用法</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/copyrun"><img data-src="holder.js/180x120" alt="" ><h4>Copy&Run</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/bugs"><img data-src="holder.js/180x120" alt="" ><h4>Bugs列表</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/tools"><img data-src="holder.js/180x120" alt="" ><h4>前端工具</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/css-resource"><img data-src="holder.js/180x120" alt="" ><h4>css资源</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/css3-base"><img data-src="holder.js/180x120" alt="" ><h4>css3属性详解</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/css3-btn"><img data-src="holder.js/180x120" alt="" ><h4>CSS3按钮</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/css3-text"><img data-src="holder.js/180x120" alt="" ><h4>CSS3文字特效</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/css3-content"><img data-src="holder.js/180x120" alt="" ><h4>CSS3生成内容</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/box-shadow"><img data-src="holder.js/180x120" alt="" ><h4>CSS3盒子阴影</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/accordion"><img data-src="holder.js/180x120" alt="" ><h4>CSS3 accordion</h4></a></li>
-                <li class="span2"><a class="thumbnail" target="_blank" href="../solutions/tabs"><img data-src="holder.js/180x120" alt="" ><h4>CSS3 tabs</h4></a></li>
-                <li class="span2"><a class="thumbnail">what's the next?<h4>敬请期待，佳作马上登场！</h4></a></li>
-            </ul>
-        </section>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/form"><img data-src="holder.js/180x120" alt="" ><h4>form表单</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/copyhtml"><img data-src="holder.js/180x120" alt="" ><h4>html固定用法</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/copyrun"><img data-src="holder.js/180x120" alt="" ><h4>Copy&Run</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/bugs"><img data-src="holder.js/180x120" alt="" ><h4>Bugs列表</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/tools"><img data-src="holder.js/180x120" alt="" ><h4>前端工具</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/css-resource"><img data-src="holder.js/180x120" alt="" ><h4>css资源</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/css3-base"><img data-src="holder.js/180x120" alt="" ><h4>css3属性详解</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/css3-btn"><img data-src="holder.js/180x120" alt="" ><h4>CSS3按钮</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/css3-text"><img data-src="holder.js/180x120" alt="" ><h4>CSS3文字特效</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/css3-content"><img data-src="holder.js/180x120" alt="" ><h4>CSS3生成内容</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/box-shadow"><img data-src="holder.js/180x120" alt="" ><h4>CSS3盒子阴影</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/accordion"><img data-src="holder.js/180x120" alt="" ><h4>CSS3 accordion</h4></a></li>
+			<li class="span2"><a class="thumbnail" target="_blank" href="../solutions/tabs"><img data-src="holder.js/180x120" alt="" ><h4>CSS3 tabs</h4></a></li>
+			<li class="span2"><a class="thumbnail">what's the next?<h4>敬请期待，佳作马上登场！</h4></a></li>
+		</ul>
+	</section>
 		
 		
 		
