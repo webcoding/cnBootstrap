943741b35eb1e1b622e754ec7afce845608cc7cc
cloudAi <cloudAi@qq.com>
2013-05-17 02:02:48 +0800

    修改

--- WF/common/meta.php
+++ WF/common/meta.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title><?php echo $title; ?> - Pandora bring</title>
+<title>Pandora - <?php echo $title; ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="<?php echo $description; ?>">
 <meta name="author" content="Pandora">
